0fb05c6f819be93648ae653c8bee3baf898c5af2
Anangtofa <149308422+Anangtofa@users.noreply.github.com>
2023-11-15 10:55:06 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,14 +29,16 @@ PREBUILT_PATH := $(DEVICE_PATH)/prebuilt
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_ABI2 := 
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := earth
@@ -145,7 +147,7 @@ TW_EXCLUDE_DEFAULT_USB_INIT := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 
-ifneq ($(OF_HIDE_NOTCH),1)
+ifeq ($(OF_HIDE_NOTCH),1)
     # Configure Status bar icons for regular TWRP builds only
     TW_CUSTOM_CLOCK_POS := 40
     TW_CUSTOM_CPU_POS := 605
